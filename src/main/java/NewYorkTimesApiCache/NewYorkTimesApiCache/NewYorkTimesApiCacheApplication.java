2f7512be98fc5312dbbec0c2e2f9ad1fcf1c281c
mikalikahi <sweeney.phil@gmail.com>
2023-01-27 11:10:53 +0800

    First commit of rebuilt news api project with maven instead of gradle to implement caching

---
+++ src/main/java/NewYorkTimesApiCache/NewYorkTimesApiCache/NewYorkTimesApiCacheApplication.java
@@ -0,0 +1,13 @@
+package NewYorkTimesApiCache.NewYorkTimesApiCache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewYorkTimesApiCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewYorkTimesApiCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NewYorkTimesApiCache/NewYorkTimesApiCache/NewYorkTimesApiCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package NewYorkTimesApiCache.NewYorkTimesApiCache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewYorkTimesApiCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
